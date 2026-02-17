ac74ecf26c47d7039c24a2d129d28b2239efd2ba
Bongane Msimanga <bongane.msimanga@vcontractor.co.za>
2026-02-17 14:29:07 +0200

    send to H5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,10 @@
+
 <script setup>
 import AppHeader from '@/components/AppHeader.vue'
 import TodoHeader from '@/components/TodoHeader.vue'
 import TodoInput from '@/components/TodoInput.vue'
 
-import { useTodoStore } from '@/stores/todoStore'
-import { onMounted } from 'vue'
-
-const todoStore = useTodoStore()
-
-onMounted(() => {
-  todoStore.fetchTodos()
-})
+const MessageFromMiniApp = "";
 
 // Send message to Mini Program.
 // my.postMessage({
@@ -22,37 +16,42 @@ onMounted(() => {
 //   },
 // });
 
-// // Receiving message from Mini Program.
-// my.onMessage = (data) => {
-//   if (data.action.type === "typeOfAction") {
-//     // "if" truthy handle and execute what needs to happen
-//   }
-// };
+// Receiving message from Mini Program.
+my.onMessage = (data) => {
+  if (data.action.type === "typeOfAction") {
+    // Handle the received message and perform corresponding operations
+    //console.log("Received message from Mini Program:", data.action.detail);
+    MessageFromMiniApp = data.action.detail;
+  }
+};
 
-// const onHandlePostMessage = () => {
-//   my.postMessage({
-//     action: {
-//       type: "typeOfAction",
-//       detail: {
-//         // Data that needs to be sent to the Mini Program
-//       },
-//     },
-//   });
-// };
+const onHandlePostMessage = () => {
+  my.postMessage({
+    action: {
+      type: "typeOfAction",
+      detail: "Message to the mini program",
+    },
+  });
+};
+
 </script>
 
 <template>
   <div class="home-page">
     <AppHeader />
     <TodoHeader />
-    <TodoInput />
+    <TodoInput /> 
     <button
         @click="onHandlePostMessage"
         class="btn btn-test"
         title="Test"
       >
-        <span class="text">Test</span>
+        <span class="text">Receive</span>
       </button>
+
+    <section>
+      <span>Message: {{ MessageFromMiniApp }}</span>
+    </section>
   </div>
 </template>
 
