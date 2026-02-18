7bd95c7adfd4782d05e49f1dbf73b10dc64f42a1
Bongane Msimanga <bongane.msimanga@vcontractor.co.za>
2026-02-18 12:26:28 +0200

    Authcode --

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,54 @@
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import AppHeader from '@/components/AppHeader.vue'
 import TodoHeader from '@/components/TodoHeader.vue'
 import TodoInput from '@/components/TodoInput.vue'
 
-const MessageFromMiniApp = "";
+// reactive holder for messages from the mini app/bridge
+const MessageFromMiniApp = ref('')
 
-// Send message to Mini Program.
-// my.postMessage({
-//   action: {
-//     type: "typeOfAction",
-//     detail: {
-//       // Data that needs to be sent to the Mini Program
-//     },
-//   },
-// });
-
-// Receiving message from Mini Program.
-my.onMessage = (data) => {
-  if (data.action.type === "typeOfAction") {
-    // Handle the received message and perform corresponding operations
-    //console.log("Received message from Mini Program:", data.action.detail);
-    MessageFromMiniApp = data.action.detail;
+// message handler (safe to assign only if bridge exists)
+const onMessageHandler = (data) => {
+  if (data?.action?.type === 'Authcode') {
+    MessageFromMiniApp.value = data.action.detail
+    console.log('Message received: ' + JSON.stringify(data.action.detail))
   }
-};
+}
+
+// Set up message handler when component mounts
+onMounted(() => {
+  if (typeof my !== 'undefined' && my !== null) {
+    try {
+      my.onMessage = onMessageHandler
+      console.log('Message handler registered on mount')
+    } catch (err) {
+      console.warn('Failed to assign my.onMessage:', err)
+    }
+  } else {
+    console.warn('Bridge `my` is not available.')
+  }
+})
+
+// my.onMessage = function (data) {
+//   if (data?.action?.type === 'Authcode') {
+//     MessageFromMiniApp.value = data.action.detail
+//     console.log('Message received: ' + JSON.stringify(data.action.detail))
+//   }
+// }
 
 const onHandlePostMessage = () => {
-  my.postMessage({
-    action: {
-      type: "typeOfAction",
-      detail: "Message to the mini program",
-    },
-  });
-};
+  if (typeof my !== 'undefined' && my && typeof my.postMessage === 'function') {
+    my.postMessage({
+      action: {
+        type: 'typeOfAction',
+        detail: 'Message to the mini program',
+      },
+    })
+  } else {
+    alert('Bridge not available: cannot post message')
+  }
+}
 
 </script>
 
@@ -50,7 +66,7 @@ const onHandlePostMessage = () => {
       </button>
 
     <section>
-      <span>Message: {{ MessageFromMiniApp }}</span>
+      <span>Authcode from mini app: {{ MessageFromMiniApp }}</span>
     </section>
   </div>
 </template>
